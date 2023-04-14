a592466f4069a3d112d44190c112de10659ff456
Michel Ribeiro Correa <michel.ribeiro@inatel.br>
2023-04-13 21:09:31 -0300

    init

---
+++ src/main/java/prova1/Michel/MichelApplication.java
@@ -0,0 +1,13 @@
+package prova1.Michel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichelApplication.class, args);
+	}
+
+}
